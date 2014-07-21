16e73cae6f493b86eccb51bcc2d63174e29d72a6
David Hill <dhill@usgs.gov>
2014-07-21 21:13:38 +0000

    committing changes for ui behavior changes to the testing/ branch
    git-svn-id: http://espa.googlecode.com/svn/testing/espa-2.4.0-release@950 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- web/ordering/views.py
+++ web/ordering/views.py
@@ -72,11 +72,13 @@ class AbstractView(View):
         if msg.lower() == 'true':
 
             ctx['display_system_message'] = True
+            
+            cache_keys = ['system_message_title',
+                          'system_message_1',
+                          'system_message_2',
+                          'system_message_3']
 
-            cache_vals = cache.get_many(['system_message_title',
-                                         'system_message_1',
-                                         'system_message_2',
-                                         'system_message_3'])
+            cache_vals = cache.get_many(cache_keys)
 
             # flag to determine if any cached values were expired/missing and
             # need to be updated
@@ -85,8 +87,8 @@ class AbstractView(View):
             c = Config()
 
             #look through the cache_vals and see if any of them are none
-            for key in cache_vals:
-                if not cache_vals[key]:
+            for key in cache_keys:
+                if not key in cache_vals:
                     update_cache = True
                     cache_vals[key] = c.get_value(key)
 
@@ -174,11 +176,25 @@ class NewOrder(AbstractView):
         if not self.scenelist:    
             if 'scenelist' in request.FILES:
                 self.scenelist = request.FILES['scenelist'].read().split('\n')
+
+
+        retval = None
         
         if self.scenelist:
-            return [d.strip() for d in self.scenelist]
-        else:
-            return None
+            for line in self.scenelist:
+                
+                line = line.strip()
+                
+                if (line.startswith("LE7") 
+                    or line.startswith("LT4") 
+                    or line.startswith("LT5")):
+                        
+                    if retval is None:
+                        retval = list()
+                        
+                    retval.append(line)
+                            
+        return retval
 
     def _get_verified_scenelist(self, request):
         sl = self._get_scenelist(request)
@@ -242,9 +258,12 @@ class NewOrder(AbstractView):
 
             for e in errors:
                 for m in e:
+                    m = m.replace("\n", "<br/>")
+                    m = m.replace("\t", "    &#149; " )
+                    m = m.replace(" ", "&nbsp;")
                     error_list.append(m)
 
-            c['errors'] = error_list
+            c['errors'] = sorted(error_list)
             c['user'] = request.user
             c['optionstyle'] = self._get_option_style(request)
 
