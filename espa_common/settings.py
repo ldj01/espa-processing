bae76e697838baeea3575769512cda6ca9df2534
David Hill <dhill@usgs.gov>
2015-01-14 15:10:11 +0000

    added another condition to errors.py/settings.py, changed the retry loop count in core.py to be >= rather than just > so it catches another iteration plus added debug to Exception clause in setting products to retry
    git-svn-id: http://espa.googlecode.com/svn/releases/espa-2.7.1-release@1589 3726ddd0-ec42-1013-fcf5-bb297c8a0eb8

--- espa_common/settings.py
+++ espa_common/settings.py
@@ -199,6 +199,7 @@ RETRY = {
     'connection_timed_out': {'timeout': 60, 'retry_limit': 5},
     'http_not_found': {'timeout': 60 * 5, 'retry_limit': 5},
     'gzip_format_error': {'timeout': 60, 'retry_limit': 2},
+    'gzip_error_eof': {'timeout': 60, 'retry_limit': 2},
     'retry_missing_l1': {'timeout': 60 * 60, 'retry_limit': 8},
     'db_lock_timeout': {'timeout':60 * 5, 'retry_limit':10},
     '502_proxy_error': {'timeout':60 * 5, 'retry_limit':10}
