b6c5dec9f1f5c95f94099e5e92f9bbcde21a2b58
Ronald Dilley <rdilley@usgs.gov>
2015-09-09 15:29:57 -0500

    Removed nlaps references and code

--- espa_common/__init__.py
+++ espa_common/__init__.py
@@ -1,6 +1,5 @@
-__all__ = ['nlaps', 'utilities', 'settings', 'sensor', 'validation', 'logger_factory']
+__all__ = ['utilities', 'settings', 'sensor', 'validation', 'logger_factory']
 
-import nlaps
 import utilities
 import settings
 import sensor
