694eef86817e9862a5197f1dcd639ac2d252d48c
Ronald Dilley <rdilley@usgs.gov>
2015-09-10 09:14:59 -0500

    reduced complexity during code cleanup from peer review

--- processing/initialization.py
+++ processing/initialization.py
@@ -4,29 +4,80 @@ License:
   "NASA Open Source Agreement 1.3"
 
 Description:
-  Provides the implementation for creating the directories used for processing.
+  Provides the implementation for creating the core directories required
+  for the processing of a product request.
 
 History:
   Created Oct/2014 by Ron Dilley, USGS/EROS
 
-    Date              Programmer               Reason
-    ----------------  ------------------------ -------------------------------
-    May/2015          Ron Dilley               Initial implementation
+    Date              Reason
+    ----------------  --------------------------------------------------------
+    May/2015          Initial implementation completed
+    Sep/2015          Reduced complexity based on peer review comments
 
 '''
 
 
 import os
 
-# imports from espa_common
-import utilities
 
-# local objects and methods
-from environment import Environment
+import utilities
+from environment import Environment, DISTRIBUTION_METHOD_LOCAL
+
+
+# ----------------------------------------------------------------------------
+def __create_local_directory(base_path, directory_name):
+    '''
+    Description:
+        Creates a local directory under the base path.
+
+    Note: "local" in this case means a standard directory.
+
+    Returns:
+        string: The fullpath to the directory created.
+
+    Parameters:
+        string: base_path - The location where to create the directory.
+        string: directory_name - The name of the directory to be created.
+    '''
+
+    full_path = os.path.join(base_path, directory_name)
+
+    utilities.create_directory(full_path)
+
+    return full_path
+
+
+# ----------------------------------------------------------------------------
+def __create_linked_directory(base_path, linked_path, link_name):
+    '''
+    Description:
+        Creates a link to the linked path
+
+    Note: "linked" in this case means a symbolic link to the linked path.
+
+    Returns:
+        string: The fullpath to the link.
+
+    Parameters:
+        string: base_path - The location where to create the directory.
+        string: linked_path - The location where the link will point.
+        string: link_name - The name of the link to be created.
+    '''
+
+    full_path = os.path.join(base_path, link_name)
+
+    utilities.create_link(linked_path, full_path)
+
+    return full_path
 
 
 # ============================================================================
-def create_local_stage_directory(base_path):
+# API Implementation
+# ============================================================================
+
+# ----------------------------------------------------------------------------
+def create_stage_directory(base_path):
     '''
     Description:
         Creates a local stage directory.
@@ -37,18 +88,14 @@ def create_local_stage_directory(base_path):
         string: The fullpath to the "stage" directory.
 
     Parameters:
-        base_path - The location where to create the "stage" directory under.
+        string: base_path - The location where to create the directory.
     '''
 
-    full_path = os.path.join(base_path, 'stage')
-
-    utilities.create_directory(full_path)
-
-    return full_path
+    return __create_local_directory(base_path, 'stage')
 
 
-# ============================================================================
-def create_local_work_directory(base_path):
+# ----------------------------------------------------------------------------
+def create_work_directory(base_path):
     '''
     Description:
         Creates a local work directory.
@@ -59,84 +106,25 @@ def create_local_work_directory(base_path):
         string: The fullpath to the "work" directory.
 
     Parameters:
-        base_path - The location where to create the "work" directory under.
+        string: base_path - The location where to create the directory.
     '''
 
-    full_path = os.path.join(base_path, 'work')
-
-    utilities.create_directory(full_path)
-
-    return full_path
+    return __create_local_directory(base_path, 'work')
 
 
-# ============================================================================
-def create_local_output_directory(base_path):
-    '''
-    Description:
-        Creates a local output directory.
-
-    Note: "local" in this case means a standard directory.
-
-    Returns:
-        string: The fullpath to the "output" directory.
-
-    Parameters:
-        base_path - The location where to create the "output" directory under.
-    '''
-
-    full_path = os.path.join(base_path, 'output')
-
-    utilities.create_directory(full_path)
-
-    return full_path
-
-
-# ============================================================================
-def create_linked_output_directory(base_path, distribution_directory):
-    '''
-    Description:
-        Creates a local output directory linked to the distribution
-        directory.  Or in other words, the online cache.
-
-    Note: "linked" in this case means a symbollic link to the online cache.
-
-    Returns:
-        string: The fullpath to the "output" link.
-
-    Parameters:
-        base_path - The location where to create the "output" link under.
-    '''
-
-    full_path = os.path.join(base_path, 'output')
-
-    utilities.create_link(distribution_directory, full_path)
-
-    return full_path
-
-
-# ============================================================================
-# API Implementation
-
-def create_stage_directory(base_path):
-    return create_local_stage_directory(base_path)
-
-
-def create_work_directory(base_path):
-    return create_local_work_directory(base_path)
-
-
+# ----------------------------------------------------------------------------
 def create_output_directory(base_path):
     '''
     Description:
         Creates either a symbolic link to the online cache or a local
         directory.
 
-    Note: With the local method, a symbolic link is created so that we can
-          just tar.gz the product and place the checksum directly on the
-          product cache.
-          With the remote method, we just create a directory to hold the
-          tar.gz and checksum before using ftp/scp to transfer the product
-          over the network.
+    Note: With the local distribution method, a symbolic link is created so
+          that we can just tar.gz the product and place the checksum directly
+          on the product (online) cache.
+          With the remote distribution method, we just create a directory to
+          hold the tar.gz and checksum before using ftp/scp to transfer the
+          product over the network.
 
     Returns:
         string: The fullpath to the "output" link or directory.
@@ -146,13 +134,13 @@ def create_output_directory(base_path):
                     directory under.
     '''
 
-    e = Environment()
+    env = Environment()
 
-    distribution_method = e.get_distribution_method()
+    distribution_method = env.get_distribution_method()
+    name = 'output'
 
-    if distribution_method == 'local':
-        distribution_directory = e.get_distribution_directory()
-        return create_linked_output_directory(base_path,
-                                              distribution_directory)
+    if distribution_method == DISTRIBUTION_METHOD_LOCAL:
+        linked_path = env.get_distribution_directory()
+        return __create_linked_directory(base_path, linked_path, name)
     else:
-        return create_local_output_directory(base_path)
+        return __create_local_directory(base_path, name)
