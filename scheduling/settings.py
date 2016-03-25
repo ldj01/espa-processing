dd439931db423ce50f82712ea163ef4a880b7ab0
Ronald Dilley <rdilley@usgs.gov>
2016-03-25 09:56:14 -0500

    Removed items not required by scheduling

--- scheduling/settings.py
+++ scheduling/settings.py
@@ -7,10 +7,6 @@
 HADOOP_MAX_JOBS = 50
 
 
-# Specifies the buffer length for an order line in the order file
-# The Hadoop File System block size should be a multiple of this value
-ORDER_BUFFER_LENGTH = 2048
-
 # Set the hadoop timeouts to a ridiculous number so jobs don't get killed
 # before they are done
 HADOOP_TIMEOUT = 172800000  # which is 2 days
@@ -24,104 +20,10 @@ HADOOP_QUEUE_MAPPING = {
     'high': 'ondemand-high'
 }
 
-# filename extension for landsat input products
-LANDSAT_INPUT_FILENAME_EXTENSION = '.tar.gz'
-
-# Path to the MODIS Terra source data location
-TERRA_BASE_SOURCE_PATH = '/MOLT'
-# Path to the MODIS Aqua source data location
-AQUA_BASE_SOURCE_PATH = '/MOLA'
-
-# file extension for modis input products
-MODIS_INPUT_FILENAME_EXTENSION = '.hdf'
-
-# Path to the completed orders
-ESPA_REMOTE_CACHE_DIRECTORY = '/data2/science_lsrd/LSRD/orders'
-ESPA_LOCAL_CACHE_DIRECTORY = ''
-
-# Number of seconds to sleep when errors are encountered before attempting the
-# task again
-DEFAULT_SLEEP_SECONDS = 2
-
-# Maximum number of times to attempt packaging, delivery, and distribution
-MAX_PACKAGING_ATTEMPTS = 3
-MAX_DELIVERY_ATTEMPTS = 3
-MAX_DISTRIBUTION_ATTEMPTS = 5
-
-# Maximum number of times to attempt setting the scene error
-MAX_SET_SCENE_ERROR_ATTEMPTS = 5
-
-# List of hostnames to choose from for the access to the online cache
-# Runs over 10Gb line
-ESPA_CACHE_HOST_LIST = ['edclxs67p', 'edclxs140p']
-
-# Specify the checksum tool and filename extension
-ESPA_CHECKSUM_TOOL = 'md5sum'
-ESPA_CHECKSUM_EXTENSION = 'md5'
-
-# Where to place the temporary scene processing log files
-LOGFILE_PATH = '/tmp'
-
-PLOT_BG_COLOR = '#f3f3f3'  # A light gray
-PLOT_MARKER = (1, 3, 0)    # Better circle than 'o'
-PLOT_MARKER_SIZE = 5.0     # A good size for the circle or diamond
-PLOT_MARKER_EDGE_WIDTH = 0.9  # The width of the black marker border
-
-# We are only supporting one radius when warping to sinusoidal
-SINUSOIDAL_SPHERE_RADIUS = 6371007.181
-
-# Some defines for common pixels sizes in decimal degrees
-DEG_FOR_30_METERS = 0.0002695
-DEG_FOR_15_METERS = (DEG_FOR_30_METERS / 2.0)
-DEG_FOR_1_METER = (DEG_FOR_30_METERS / 30.0)
-
-# Supported datums - the strings for them
-WGS84 = 'WGS84'
-NAD27 = 'NAD27'
-NAD83 = 'NAD83'
-# WGS84 should always be first in the list
-VALID_DATUMS = [WGS84, NAD27, NAD83]
-
-TRANSFER_BLOCK_SIZE = 10485760
-
-# We do not allow any user selectable choices for this projection
-GEOGRAPHIC_PROJ4_STRING = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-
-# Band type data ranges.  They are intended to be used for removing outliers
-# from the data before statistics generation
-# Must match DATA_MAX_Y and DATA_MIN_Y values in plotting.py
-# The types must match the types in cdr_ecv.py and modis.py
-# Note: These are also defined in such away that the fill values are also
-#       excluded.
-BAND_TYPE_STAT_RANGES = {
-    'SR': {
-        'UPPER_BOUND': 10000,
-        'LOWER_BOUND': 0
-    },
-    'TOA': {
-        'UPPER_BOUND': 10000,
-        'LOWER_BOUND': 0
-    },
-    'INDEX': {
-        'UPPER_BOUND': 10000,
-        'LOWER_BOUND': -1000
-    },
-    'LST': {
-        'UPPER_BOUND': 65535,
-        'LOWER_BOUND': 7500
-    },
-    'EMIS': {
-        'UPPER_BOUND': 255,
-        'LOWER_BOUND': 1
-    }
-}
-
 
 '''
 LOGGING DEFINITIONS
 '''
-PROCESSING_LOGGER = 'espa.processing'
-
 LOGGER_CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -205,13 +107,6 @@ LOGGER_CONFIG = {
             'filename': '/tmp/espa-plot-cron.log',
             'mode': 'a'
         },
-        'espa.cron.orderdisp': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'formatter': 'espa.standard',
-            'filename': '/tmp/espa-order-disposition-cron.log',
-            'mode': 'a'
-        },
         'espa.processing': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
@@ -253,12 +148,6 @@ LOGGER_CONFIG = {
             'level': 'INFO',
             'propagate': False,
             'handlers': ['espa.cron.plot']
-        },
-        'espa.cron.orderdisp': {
-            # To be used by the order disposition cron
-            'level': 'INFO',
-            'propagate': False,
-            'handlers': ['espa.cron.orderdisp']
         },
         'espa.processing': {
             # To be used by the processing system
