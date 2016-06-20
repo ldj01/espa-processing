eb2877fc70d015614f894f2f4342086de532a2c0
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2016-06-20 11:55:16 -0500

    collection processing updates

--- processing/test-orders/run-ondemand-request.py
+++ processing/test-orders/run-ondemand-request.py
@@ -290,6 +290,52 @@ def export_environment_variables(cfg):
         os.environ[key.upper()] = value
 
 
+def get_cfg_file_path(filename):
+    """Build the full path to the config file
+
+    Args:
+        filename <str>: The name of the file to append to the full path
+
+    Raises:
+        Exception(<str>)
+    """
+
+    # Use the users home directory as the base source directory for
+    # configuration
+    if 'HOME' not in os.environ:
+        raise Exception('[HOME] not found in environment')
+    home_dir = os.environ.get('HOME')
+
+    # Build the full path to the configuration file
+    config_path = os.path.join(home_dir, '.usgs', 'espa', filename)
+
+    return config_path
+
+
+def retrieve_cfg(cfg_filename):
+    """Retrieve the configuration for the cron
+
+    Returns:
+        cfg <ConfigParser>: Configuration for ESPA cron
+
+    Raises:
+        Exception(<str>)
+    """
+
+    # Build the full path to the configuration file
+    config_path = get_cfg_file_path(cfg_filename)
+
+    if not os.path.isfile(config_path):
+        raise Exception('Missing configuration file [{}]'
+                        .format(config_path))
+
+    # Create the object and load the configuration
+    cfg = ConfigParser()
+    cfg.read(config_path)
+
+    return cfg
+
+
 def main():
     """Main code for executing a test order
     """
@@ -307,8 +353,7 @@ def main():
     parser = build_argument_parser()
 
     # Load up the environment variables from the processing configuration
-    cfg = ConfigParser()
-    cfg.read('processing.conf')
+    cfg = retrieve_cfg('processing.conf')
 
     export_environment_variables(cfg)
 
