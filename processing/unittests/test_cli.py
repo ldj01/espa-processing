65102d69db51d2ffff40d3db672029205dc3bc3d
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2016-07-26 11:59:20 -0500

    Added

---
+++ processing/unittests/test_cli.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+import os
+import unittest
+
+
+import cli
+
+
+class TestCLI(unittest.TestCase):
+    """Test the cli.py methods"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_one(self):
+        self.assertTrue(True)
